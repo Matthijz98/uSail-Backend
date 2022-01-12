a0c78b2fcc9ec34181ef929808b729b43d9a6763
KeithMarex <koenvandermarel@gmail.com>
2022-01-12 16:05:38 +0100

    Me being dumb

--- app/seeders/20220112123729-demo-boats.js
+++ app/seeders/20220112123729-demo-boats.js
@@ -5,8 +5,8 @@ module.exports = {
      await queryInterface.bulkInsert('Boats', [{
        "id": "0a70658f-04e0-4be1-84ae-02656eb13406",
        "boat_name": "test boat name",
-       "boat_location": "boat location coordinates",
-       "boat_active_user": "user which is currently using the boat",
+       "boat_location": "coordinate",
+       "boat_active_user": "b80ca1c9-6cc7-4723-a92c-940adea582d7",
        "boat_image": null,
        "boat_description": "This boat is a great boat for testing",
        "createdAt": new Date(),

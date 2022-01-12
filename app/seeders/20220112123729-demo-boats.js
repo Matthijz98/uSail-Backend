2c6b2fe71df3c0556fbb62b40895594aa206d342
KeithMarex <koenvandermarel@gmail.com>
2022-01-12 14:54:39 +0100

    Seedeers update

--- app/seeders/20220112123729-demo-boats.js
+++ app/seeders/20220112123729-demo-boats.js
@@ -5,6 +5,8 @@ module.exports = {
      await queryInterface.bulkInsert('Boats', [{
        "id": "0a70658f-04e0-4be1-84ae-02656eb13406",
        "boat_name": "test boat name",
+       "boat_location": "boat location coordinates",
+       "boat_active_user": "user which is currently using the boat",
        "boat_image": null,
        "boat_description": "This boat is a great boat for testing",
        "createdAt": new Date(),

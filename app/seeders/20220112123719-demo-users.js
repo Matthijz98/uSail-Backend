cc37deb07db55b0481e40a322e01a676df881185
Matthijs <mzondervan@hotmail.nl>
2022-01-31 13:28:50 +0100

    clean up the default users

--- app/seeders/20220112123719-demo-users.js
+++ app/seeders/20220112123719-demo-users.js
@@ -5,14 +5,14 @@ module.exports = {
     await queryInterface.bulkInsert('Users', [{
       "id": "57a8e76e-5c06-4078-a81c-4f7695b0c3b4",
       "user_password": "testpass",
-      "user_email": "test@test.com",
+      "user_email": "matthijs@test.com",
       "user_name": "matthijs",
       "createdAt": new Date(),
       "updatedAt": new Date()
     },{
       "id": "b80ca1c9-6cc7-4723-a92c-940adea582d7",
-      "user_password": "hoiioh",
-      "user_email": "lol",
+      "user_password": "password",
+      "user_email": "koen@test.com",
       "user_name": "koen",
       "createdAt": new Date(),
       "updatedAt": new Date()
